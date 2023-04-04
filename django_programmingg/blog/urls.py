ea2cfde4813de62974f644f7596e307b93e760d1
Hyeanwoo Yang <hwyang0919@naver.com>
2023-04-04 18:02:10 +0900

    5주차 컷

--- django_programmingg/blog/urls.py
+++ django_programmingg/blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     # path('', views.index),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
+    path('category/<str:slug>/', views.categories_page),
+    path('tag/<str:slug>/', views.tag_page),
     # path('<int:post_num>/', views.single_post_page),
 ]
