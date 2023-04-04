b43a09c6389d03ccdb4f8fc32163a044c5bef2e5
Hyeanwoo Yang <hwyang0919@naver.com>
2023-04-04 12:59:42 +0900

    url 패턴 매칭, FBV/CBV뷰 생성

--- django_programmingg/blog/urls.py
+++ django_programmingg/blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('<int:post_num>/', views.single_post_page),
+    # path('', views.index),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
+    # path('<int:post_num>/', views.single_post_page),
 ]
