fda972afc93d426260ab13136dd7a24cca28e97c
Hyeanwoo Yang <hwyang0919@naver.com>
2023-04-18 15:45:49 +0900

    마크다운, 폼 추가

--- django_programmingg/urls.py
+++ django_programmingg/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('markdownx/', include('markdownx.urls')),
     path('', include('single_pages.urls')),
 ]
 
