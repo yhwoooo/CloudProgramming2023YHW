880fb39a764e7f25d55a4a369cccc1bd30d1a885
Hyeanwoo Yang <hwyang0919@naver.com>
2023-04-18 17:46:48 +0900

    댓글 관련 기능 추가

--- django_programmingg/blog/urls.py
+++ django_programmingg/blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('update_post/<int:pk>', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('<int:pk>/add_comment/', views.add_comment),
     path('', views.PostList.as_view()),
     path('category/<str:slug>/', views.categories_page),
     path('tag/<str:slug>/', views.tag_page),
